c5c718ca848d24ff79645030576d6198d39c2d39
paulinehadad <paulinehadad@Paulines-MacBook-Pro.local>
2014-10-15 01:31:15 -0400

    Cleaned code+created SASS

--- scripts/guide.js
+++ scripts/guide.js
@@ -1,14 +1,3 @@
-
-var feed = new Instafeed({
-        get: 'tagged',
-        tagName: 'nycfood',
-        clientId: '40fd8ee862024a5d86ccca9bdd9c36da'
-});
-feed.run();
-    access_parameters = {
-        access_token: access_token
-    };
-
 var Food = {
     index: window.localStorage.getItem("food:index"),
     $table: document.getElementById("food-table"),
